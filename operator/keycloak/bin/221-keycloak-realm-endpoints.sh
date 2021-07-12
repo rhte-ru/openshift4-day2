f08bff4f386f75a6bfebdb2c37a6ce180c5461f4
dsevosty <obudick@yandex.ru>
2021-07-12 12:12:21 +0300

    Update 221-keycloak-realm-endpoints.sh

--- operator/keycloak/bin/221-keycloak-realm-endpoints.sh
+++ operator/keycloak/bin/221-keycloak-realm-endpoints.sh
@@ -7,4 +7,4 @@ base=$(realpath -e $(dirname $BASH_SOURCE)/..)
 NS=${NS:-namespace-example}
 REALM_ID=${REALM_ID:-ldap-realm-example}
 
-curl -kL $($base/bin/216-keycloak-admin-endpoint.sh)/auth/realms/$REALM_ID/.well-known/openid-configuration 2> /dev/null
+curl -kL $(bash $base/bin/216-keycloak-admin-endpoint.sh)/auth/realms/$REALM_ID/.well-known/openid-configuration 2> /dev/null
