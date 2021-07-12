62ed0b9ca17076bfe087a731bb2a71bd2746fa5f
dsevosty <obudick@yandex.ru>
2021-07-12 12:14:00 +0300

    Update 220-patch-proxy-ca.sh

--- config/tls/bin/220-patch-proxy-ca.sh
+++ config/tls/bin/220-patch-proxy-ca.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DRY_RUN=${DRY_RUN:-true}
+DRY_RUN=${DRY_RUN:-client}
 TRUSTED_CA_CM=${TRUSTED_CA_CM:-user-ca-bundle}
 
 oc patch --dry-run=$DRY_RUN --type merge proxy/cluster -p "{ \"spec\": { \"trustedCA\": { \"name\": \"$TRUSTED_CA_CM\" } } }" 
