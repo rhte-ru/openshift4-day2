3341e17ec93977dfde9590ade57d1116a4de7864
OpenShift Admin user <ocp-admin@master.dsevosty.test>
2021-07-14 18:03:47 +0300

    usage added

--- config/auth/bin/205-create-idp-secret.sh
+++ config/auth/bin/205-create-idp-secret.sh
@@ -7,6 +7,11 @@ base=$(realpath -e $(dirname $BASH_SOURCE)/..)
 IDP_CLIENT_SECRET=${IDP_CLIENT_SECRET:-idp-secret-example}
 IDP_CLIENT_PASSWORD=${IDP_CLIENT_PASSWORD:-$1}
 
+if [ -z "$1" ] ; then
+    echo "SYNTAX: $BASH_SOURCE <idp-bind-user-password>"
+    exit 1
+fi
+
 oc -n openshift-config create secret generic \
     $IDP_CLIENT_SECRET \
     --from-literal=bindPassword="$IDP_CLIENT_PASSWORD" \
